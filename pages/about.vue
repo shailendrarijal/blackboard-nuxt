10dc95b25f630e1a31f11ea34d8f16a607d7b9ce
shailendra <shailendra@agures.com>
2022-08-20 17:52:16 +1000

    fix logo

--- pages/about.vue
+++ pages/about.vue
@@ -9,11 +9,6 @@
       <template v-slot:default>
         <div class="content">
           <h3>About Blackboard Nepal</h3>
-          /*<p>
-            <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
-            </strong>
-          </p>*/
           <p>
             Blackboard Nepal was born with an idea of empowering the community. We beleive that the best way of empowerment is through information. We will be writing on topics such as Technology, Language and Literature, Health and nutrition, Finance and so on
           </p>
@@ -21,48 +16,6 @@
             You can search for articles using the search bar on the top or go on categories and browse the categories
           </p>
         </div>
-
-        /*<div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Cool Stuff We Do
-              </p>
-              <p class="subtitle">
-                Sooo Cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Other Cool Things
-              </p>
-              <p class="subtitle">
-                Just as cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                All the cool
-              </p>
-              <p class="subtitle">
-                that is all
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-        </div>*/
       </template>
 
       <template v-slot:sidebar>
